ab826d2c0a71164424b9ef82073abc01ae5cd186
Ricardo Arend <ricardoarend@gmail.com>
2019-09-02 15:00:58 -0300

    teste
    teste

--- pagina/consulta_bd.php
+++ pagina/consulta_bd.php
@@ -41,14 +41,14 @@ expulsaVisitante();
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="http://localhost/pagina/main.php">Home</a>
+      <a class="navbar-brand" href="main.php">Home</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="http://localhost/pagina/upload.php">Upload</a></li>
-        <li class="active"><a href="http://localhost/pagina/consulta.php">Consulta</a></li>
+        <li><a href="upload.php">Upload</a></li>
+        <li class="active"><a href="consulta.php">Consulta</a></li>
       </ul>
       </ul><form method="GET" action="consulta_bd.php" class="navbar-form navbar-left" role="search">
         <div class="form-group"> 
--- pagina/main.php
+++ pagina/main.php
@@ -44,14 +44,14 @@ if(isset($_POST["botao"])){
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="http://localhost/pagina/main.php">Home</a>
+      <a class="navbar-brand" href="main.php">Home</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="http://localhost/pagina/upload.php">Upload</a></li>
-        <li><a href="http://localhost/pagina/consulta.php">Consulta</a></li>
+        <li><a href="upload.php">Upload</a></li>
+        <li><a href="consulta.php">Consulta</a></li>
       </ul> 
      <ul class="nav navbar-nav navbar-right">
        <div class="control-group">
